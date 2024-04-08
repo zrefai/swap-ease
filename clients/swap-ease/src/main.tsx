d97c5453c36bb59039c5613fea487ce3318da6c0
Zaki <zkrefai@gmail.com>
2024-04-07 20:40:48 -0500

    Add minimal vite app

---
+++ clients/swap-ease/src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+
+const Root = () => {
+  return <div>hello</div>;
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </React.StrictMode>
+);
